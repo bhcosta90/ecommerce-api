ad4631ac8e633748db8ba1a8ef3104f4e5f9574c
Bruno Costa <bhcosta90@gmail.com>
2025-04-26 15:55:00 -0300

    Implement AsApiResource trait and Department resource handling

--- app/Http/Controllers/V1/DepartmentController.php
+++ app/Http/Controllers/V1/DepartmentController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\DepartmentResource;
+use App\Models\Department;
+use Costa\Package\Controller\AsApiResource;
 
 class DepartmentController extends Controller
 {
-    public function index()
-    {
+    use AsApiResource;
 
+    protected function model(): string
+    {
+        return Department::class;
+    }
+
+    protected function resource(): string
+    {
+        return DepartmentResource::class;
     }
 }
